9a27277fb24877b732f6b8f98313f0115d608808
Jose Luis Crisostomo <jl.cs606@gmail.com>
2016-05-07 13:56:24 -0400

    ShareAction Rpovider enabled

--- app/src/main/java/devdreamers/dev/forecastapp/Activities/DetailActivity.java
+++ app/src/main/java/devdreamers/dev/forecastapp/Activities/DetailActivity.java
@@ -4,10 +4,13 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,14 +20,13 @@ import devdreamers.dev.forecastapp.R;
 
 public class DetailActivity extends AppCompatActivity {
 
-    //variables
-    private ShareActionProvider mShareActionProvider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         setSupportActionBar(toolbar);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
@@ -34,37 +36,19 @@ public class DetailActivity extends AppCompatActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.detail, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Intent intent = new Intent(this,SettingsActivity.class);
-            startActivity(intent);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     /**
      * A placeholder fragment containing a simple view.
      */
     public static class PlaceholderFragment extends Fragment {
+        //variables
+        private ShareActionProvider mShareActionProvider;
+        private static String LOG_TAG = PlaceholderFragment.class.getSimpleName();
+        private static final String FORECAST_SHARE_HASHTAG = "#SunshineApp";
+        private String mForecastStr;
 
         private TextView daily;
         public PlaceholderFragment() {
+            setHasOptionsMenu(true);
         }
 
         @Override
@@ -75,11 +59,59 @@ public class DetailActivity extends AppCompatActivity {
             // The detail Activity called via intent.  Inspect the intent for forecast data.
                        Intent intent = getActivity().getIntent();
                         if (intent != null && intent.hasExtra(Intent.EXTRA_TEXT)) {
-                                String forecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
+                                mForecastStr = intent.getStringExtra(Intent.EXTRA_TEXT);
                                 ((TextView) rootView.findViewById(R.id.forecast_day_textview))
-                                                .setText(forecastStr);
+                                                .setText(mForecastStr);
                             }
             return rootView;
         }
+
+
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            inflater.inflate(R.menu.detail,menu);
+
+            //geting the option share
+            MenuItem item = menu.findItem(R.id.menu_item_share);
+            //handling the actions share
+            ShareActionProvider mShareActionRpovider =
+                    (ShareActionProvider) MenuItemCompat.getActionProvider(item);
+            // Attach an intent to this ShareActionProvider.  You can update this at any time,
+            // like when the user selects a new piece of data they might like to share.
+            if(mShareActionRpovider != null){
+                //calling a function to get the intent which is going to perfom the action
+                mShareActionRpovider.setShareIntent(createShareForecastIntent());
+            }
+            else {
+                Log.d(LOG_TAG, "Share Action Provider is null?");
+            }
+        }
+
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
+
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                Intent intent = new Intent(getActivity(),SettingsActivity.class);
+                startActivity(intent);
+                return true;
+            }
+            return true;
+        }
+
+        private Intent createShareForecastIntent() {
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_TEXT,mForecastStr + FORECAST_SHARE_HASHTAG);
+            return shareIntent;
+
+        }
+
     }
 }
